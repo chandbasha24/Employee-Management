3618491b0b7c0e7a306deb2c0c697077bf13bfdc
Chand Basha Panyam <chandpanyam54@gmail.com>
2024-09-22 22:17:54 +0530

    Initial commit with frontend and backend

---
+++ frontend/src/Components/Login.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    navigate('/employee')
+    try {
+      const res = await axios.post('http://localhost:8080/api/auth/login', {
+        f_userName: username,
+        f_Pwd: password,
+      });
+      // Store token in localStorage
+      localStorage.setItem('token', res.data.token);
+      navigate('/employees'); // Redirect to Employee List after login
+    } catch (err) {
+      setError(err.response?.data?.message || 'Login failed. Please try again.');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Username:</label>
+          <input 
+            type="text" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+            required 
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input 
+            type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            required 
+          />
+        </div>
+        
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
